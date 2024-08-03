f95181560da21a62e3f0b85d735aa31c1307b548
Antonina <antoninakot95@gmail.com>
2024-08-03 14:12:02 +0300

    Init

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+/* eslint-env node */
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript/recommended',
+    '@vue/eslint-config-prettier',
+    './.eslintrc-auto-import.json',
+  ],
+  env: {
+    'vue/setup-compiler-macros': true,
+    node: true,
+  },
+}
---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { createApp } from 'vue'
+
+
+
+import App from './App.vue'
+
+
+// css
+import './assets/sass/app.scss'
+import './assets/css/vendor.css'
+
+const app = createApp(App)
+
+app.mount('#app')
